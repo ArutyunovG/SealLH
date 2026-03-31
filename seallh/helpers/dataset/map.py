c8b9e0016fde0d9b6c864a2098e52578d93157d1
Grigory Arutyunov <arutyunovgrigory@gmail.com>
2026-03-31 17:50:17 +0300

    add missing map.py

---
+++ seallh/helpers/dataset/map.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset
+
+from typing import Callable
+
+class Map(Dataset):
+
+    def __init__(self, 
+                 dataset: Dataset,
+                 func: Callable):
+
+        assert isinstance(dataset, Dataset)
+        assert callable(func)
+
+        self.dataset = dataset
+        self.func = func
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+    def __getitem__(self, idx: int):
+        return self.func(self.dataset[idx])
